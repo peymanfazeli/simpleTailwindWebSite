21db5fa680d7dae6b78be0a9498f2e9ab77547d4
peymanfazeli <peymanfazeli.work@gmail.com>
2023-02-14 16:48:05 +0330

    adding intersection and loaing sections & lazy image loading

--- script.js
+++ script.js
@@ -9,3 +9,41 @@ btns.forEach((btn) =>
     dist.scrollIntoView({ behavior: "smooth" });
   })
 );
+// making sections hidden
+const allSections = document.querySelectorAll(".section");
+const revealSections = (thrEntries, observer) => {
+  const [thrEntry] = thrEntries;
+  if (!thrEntry.isIntersecting) return;
+  thrEntry.target.classList.remove("translate-y-32");
+  thrEntry.target.classList.remove("opacity-0");
+  observer.unobserve(thrEntry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSections, {
+  root: null,
+  threshold: 0.1,
+});
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("opacity-0");
+  section.classList.add("transform");
+  section.classList.add("translate-y-32");
+});
+// Loading lazy img
+const imgTarget = document.querySelectorAll("img[data-src]");
+const imgLoader = (thrEntries, observer) => {
+  const [thrEntry] = thrEntries;
+  if (!thrEntry.isIntersecting) return;
+  thrEntry.target.src = thrEntry.target.dataset.src;
+  thrEntry.target.addEventListener("load", () => {
+    thrEntry.target.classList.remove("blur-lg");
+  });
+  observer.unobserve(thrEntry.target);
+};
+const imgObserver = new IntersectionObserver(imgLoader, {
+  root: null,
+  threshold: 0.9,
+  rootMargin: "500px",
+});
+imgTarget.forEach((img) => {
+  imgObserver.observe(img);
+});
